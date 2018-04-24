5afa51af3f10fdb46ed7c4e688275838f0e24122
arashVahidi <arash.v60@gmail.com>
2018-04-24 12:56:19 +0430

    Commite test

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        // add only for test
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
