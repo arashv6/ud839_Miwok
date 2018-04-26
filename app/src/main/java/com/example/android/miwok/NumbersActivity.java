329a5721b8d29d218c9d41bd13e791f17211cf03
arashVahidi <arash.v60@gmail.com>
2018-04-26 14:20:02 +0430

    update to Number2 of lesson 2

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,14 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +32,41 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        //create arralist
+        ArrayList<String> myList= new ArrayList<String>();
+        myList.add("one");
+        myList.add("two");
+        myList.add("three");
+        myList.add("four");
+        myList.add("five");
+        myList.add("six");
+        myList.add("seven");
+        myList.add("eight");
+        myList.add("nine");
+        myList.add("ten");
+        myList.add("eleven");
+        myList.add("twelve");
+        myList.add("therein");
+        /*
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myList);
+        ListView myListView = (ListView) findViewById(R.id.idrootLayout);
+        myListView.setAdapter(myAdapter);
+        */
+
+//        LinearLayout nrootview = (LinearLayout) findViewById(R.id.idrootLayout);w
+//        for (int x=0; x<myList.size(); ++x )
+//        {
+//            TextView listview = new TextView(this);
+//            listview.setText(myList.get(x));
+//            nrootview.addView(listview);
+//        }
+        ArrayAdapter<String> gridAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,myList);
+        GridView mygv = (GridView) findViewById(R.id.idgridlist);
+        mygv.setAdapter(gridAdapter);
+        Log.v("NumbersActivity",  "List0: " + myList.get(0) );
+        Log.v("NumbersActivity",  "List1: " + myList.get(1) );
+        //
+
+
     }
 }
